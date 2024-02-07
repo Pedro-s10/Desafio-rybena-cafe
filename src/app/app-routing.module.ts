4d875d53f337f24f458bbe873168a49cdffd50f7
pedro <pedrorxrxx@gmail.com>
2024-02-07 15:56:58 -0300

    feat: created router typical-dishes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AboutCoffeComponent} from "./pages/about-coffe/about-coffe.component";
+import {TypicalDishesComponent} from "./pages/typical-dishes/typical-dishes.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,11 @@ const routes: Routes = [
   {
     path: 'sobre',
     component: AboutCoffeComponent
+  },
+
+  {
+    path: 'pratos-tipicos',
+    component: TypicalDishesComponent
   },
 ];
 
