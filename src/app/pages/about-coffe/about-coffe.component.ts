8589cc14b6743213630360e4867b673b39505919
pedro <pedrorxrxx@gmail.com>
2024-02-07 16:48:14 -0300

    fix: refactoring implementation using for

--- src/app/pages/about-coffe/about-coffe.component.ts
+++ src/app/pages/about-coffe/about-coffe.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+export interface ICardData {
+  pathImage: string
+  matTooltip: string
+  alt: string
+  title: string
+}
+
 @Component({
   selector: 'app-about-coffe',
   templateUrl: './about-coffe.component.html',
   styleUrls: ['./about-coffe.component.css']
 })
 export class AboutCoffeComponent {
+  cards: ICardData[] = [
+    {
+      alt: 'Café Arábica',
+      matTooltip: 'Café Arábica',
+      title: 'Café Arábica',
+      pathImage: 'https://www.graogourmet.com/wp-content/uploads/2022/02/Capas-blog-cafe-arabica.jpg'
+    },
 
+    {
+      alt: 'Café Bourbon',
+      matTooltip: 'Café Bourbon',
+      title: 'Café Bourbon',
+      pathImage: 'https://www.graogourmet.com/wp-content/uploads/2016/02/bourbon-amarelo-oficial.jpg'
+    },
+
+    {
+      alt: 'Café Kona',
+      matTooltip: 'Café Kona',
+      title: 'Café Kona',
+      pathImage: 'https://img.freepik.com/fotos-premium/graos-de-cafe-havaianos-kona-red-na-arvore-que-cresce-na-plantacao-em-kauai_539233-5338.jpg'
+    },
+
+    {
+      alt: 'Café Geisha',
+      matTooltip: 'Café Geisha',
+      title: 'Café Geisha',
+      pathImage: 'https://dailycoffeenews.com/wp-content/uploads/2017/11/Geisha-Cherries-01-Chispita-Costa-Rica.jpg'
+    },
+
+  ]
 }
