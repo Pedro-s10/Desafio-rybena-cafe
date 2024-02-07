879e49f52ba8bcd5694aee9c0456b6ccd1732453
pedro <pedrorxrxx@gmail.com>
2024-02-07 17:40:51 -0300

    feat: add button

--- src/app/pages/typical-dishes/typical-dishes.component.ts
+++ src/app/pages/typical-dishes/typical-dishes.component.ts
@@ -5,6 +5,7 @@ export interface ICardData {
   matTooltip: string
   alt: string
   title: string
+  openModal: Function
 }
 
 @Component({
@@ -19,28 +20,40 @@ export class TypicalDishesComponent {
       alt: 'Mousse de café',
       matTooltip: 'Mousse de café',
       title: 'Mousse de café',
-      pathImage: 'https://reservagourmet.files.wordpress.com/2014/10/recipe2069_mouskafe.jpg?w=604'
+      pathImage: 'https://reservagourmet.files.wordpress.com/2014/10/recipe2069_mouskafe.jpg?w=604',
+      openModal: () => {
+
+      }
     },
 
     {
       alt: 'Gelato de café',
       matTooltip: 'Gelato de café',
       title: 'Gelato de café',
-      pathImage: 'https://f.i.uol.com.br/guia/bares/images/15294750.jpeg'
+      pathImage: 'https://f.i.uol.com.br/guia/bares/images/15294750.jpeg',
+      openModal: () => {
+
+      }
     },
 
     {
       alt: 'Filé ao molho de café',
       matTooltip: 'Filé ao molho de café',
       title: 'Filé ao molho de café',
-      pathImage: 'https://www.boqnews.com/wp-content/uploads/2021/08/receita_de_fil_mignon_ao_molho_madeira_2020-06-09.jpeg'
+      pathImage: 'https://www.boqnews.com/wp-content/uploads/2021/08/receita_de_fil_mignon_ao_molho_madeira_2020-06-09.jpeg',
+      openModal: () => {
+
+      }
     },
 
     {
       alt: 'Gelatina de café',
       matTooltip: 'Gelatina de café',
       title: 'Gelatina de café',
-      pathImage: 'https://gelialgas.com.br/wp-content/uploads/2020/06/3a.jpg'
+      pathImage: 'https://gelialgas.com.br/wp-content/uploads/2020/06/3a.jpg',
+      openModal: () => {
+
+      }
     },
 
   ]
