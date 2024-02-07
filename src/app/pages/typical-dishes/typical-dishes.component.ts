2eb551f7d06b94047173cbf0d2ce897e2091538f
pedro <pedrorxrxx@gmail.com>
2024-02-07 16:32:55 -0300

    fix: refactoring implementation using for

--- src/app/pages/typical-dishes/typical-dishes.component.ts
+++ src/app/pages/typical-dishes/typical-dishes.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface ICardData {
+  pathImage: string
+  matTooltip: string
+  alt: string
+  title: string
+}
 
 @Component({
   selector: 'app-typical-dishes',
@@ -7,4 +14,34 @@ import { Component } from '@angular/core';
 })
 export class TypicalDishesComponent {
 
+  cards: ICardData[] = [
+    {
+      alt: 'Mousse de café',
+      matTooltip: 'Mousse de café',
+      title: 'Mousse de café',
+      pathImage: 'https://reservagourmet.files.wordpress.com/2014/10/recipe2069_mouskafe.jpg?w=604'
+    },
+
+    {
+      alt: 'Gelato de café',
+      matTooltip: 'Gelato de café',
+      title: 'Gelato de café',
+      pathImage: 'https://f.i.uol.com.br/guia/bares/images/15294750.jpeg'
+    },
+
+    {
+      alt: 'Filé ao molho de café',
+      matTooltip: 'Filé ao molho de café',
+      title: 'Filé ao molho de café',
+      pathImage: 'https://www.boqnews.com/wp-content/uploads/2021/08/receita_de_fil_mignon_ao_molho_madeira_2020-06-09.jpeg'
+    },
+
+    {
+      alt: 'Gelatina de café',
+      matTooltip: 'Gelatina de café',
+      title: 'Gelatina de café',
+      pathImage: 'https://gelialgas.com.br/wp-content/uploads/2020/06/3a.jpg'
+    },
+
+  ]
 }
